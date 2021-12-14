701ca04eb9917482cba39e77fecaa563436a1e1c
murray22 <murray22@demeter0.multi-client.sosp21ae-pg0.utah.cloudlab.us>
2021-12-14 16:53:02 -0700

    Demo simulation has been updated with last two policies. Currently updating to make it cleaner

--- simulator/demo_simulation.cpp
+++ simulator/demo_simulation.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <sstream>
 #include <fcntl.h>
@@ -13,26 +14,42 @@
 #include <time.h>
 #include <set>
 #include "mem.h"
+#include <map>
 
 #define TWOMB 2000000
 #define MAX_ENTRIES 2000000/8192
 #define NUM_LINES 1000000
 #define VAL_SIZE 8192
 size_t number_of_pages = 0;
-std::string trace_file = "cluster001"; //"synthetic_twemcache.csv";
+std::string trace_file = "cluster001";//"synthetic_twemcache_1000.csv";
+std::vector<size_t> second_check = {10};
+std::vector<size_t> threshold_discard = {1};
+
 //"cluster001";
 // Basic facts: Page is 2MB, Assume each key is 
+
+/* General Purpose Data structures*/
 std::unordered_map<size_t, std::vector<std::string>> offline_hot_keys;
 std::unordered_map<size_t, std::vector<std::string>> page_to_key_map;
 std::unordered_map<std::string, size_t> key_to_page_map;
+std::unordered_map<size_t, size_t> client_to_page_map;
 std::unordered_map<size_t, std::vector<std::string>> page_to_client_map;
+std::map<size_t, size_t> clients;
+std::map<std::string, size_t> keys;
 
+/* LRU Policy Data structures */
+
+/* Popular Client Policy Data structures */
+
+/* Hot Key Policy Data strucutres */
+
+/* Random Policy Data structures */
 
 void print_hot_keys(void);
 size_t convert_to_sizet(std::string str_size_t);
 int test_policy_one();
-/*int test_policy_two();
-int test_policy_three();*/
+int test_policy_two();
+int test_policy_three();
 int test_policy_four();
 size_t execute_trace(std::unordered_map<std::string, size_t> kv_store);
 size_t offline_hot_key_distribution(std::string trace_file);
@@ -40,6 +57,26 @@ std::unordered_map<std::string, size_t> fill_pinned_pages(std::string trace_file
 size_t count_lines(std::string trace_file);
 std::vector<std::string> parse_line(std::string line);
 std::string get_val(std::string bytes_str);
+
+int good_operation(std::string op) {
+  return op.compare("get") != 0;
+}
+
+bool cmp(std::pair<size_t, int>& a,
+         std::pair<size_t, int>& b){
+    return a.second < b.second;
+}
+
+std::pair<size_t,size_t> flip_pair(const std::pair<size_t,size_t> &p) {
+    return std::pair<size_t,size_t>(p.second, p.first);
+}
+
+std::map<size_t,size_t> flip_map(const std::map<size_t,size_t> &src) {
+    std::map<size_t,size_t> dst;
+    std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()), 
+                   flip_pair);
+    return dst;
+}
 
 void assign_pages() {
   std::ifstream trace;
@@ -50,7 +87,7 @@ void assign_pages() {
   if (trace.is_open()) {
     while (getline(trace, line)) {
       std::vector<std::string> twitter_req = parse_line(line);
-      if (twitter_req[5].compare("gets") != 0 || key_to_page_map.find(twitter_req[1]) != key_to_page_map.end()) {
+      if (twitter_req[5].compare("get") != 0 || key_to_page_map.find(twitter_req[1]) != key_to_page_map.end()) {
         continue;
       }
       if (current_page == 0 || num_slots == 0) {
@@ -59,34 +96,96 @@ void assign_pages() {
 	new_vec.push_back(twitter_req[1]);
         page_to_key_map.insert(std::pair<size_t, std::vector<std::string>>(current_page, new_vec));
 	key_to_page_map.insert(std::pair<std::string, size_t>(twitter_req[1], current_page));
+	client_to_page_map.insert(std::pair<size_t, size_t>(convert_to_sizet(twitter_req[4]), current_page));
 	num_slots = MAX_ENTRIES - 1;
       } else {
 	page_to_key_map[current_page].push_back(twitter_req[1]);
 	key_to_page_map.insert(std::pair<std::string, size_t>(twitter_req[1], current_page));
+        client_to_page_map.insert(std::pair<size_t, size_t>(convert_to_sizet(twitter_req[4]), current_page));
 	num_slots -= 1;
       }
-      std::cout << "Current page: " << current_page << " and key " << twitter_req[1] << std::endl;
+      //std::cout << "Current page: " << current_page << " and key " << twitter_req[1] << std::endl;
     }
   }
   number_of_pages = current_page;
   trace.close();
 }
 
+int test_policy(int policy_no) {
+  std::ifstream trace;
+  std::ofstream pinning;
+  std::ofstream perf;
+  trace.open(trace_file);
+  if (policy_no == 1) { // LRU
+    pinning.open("pinning1.csv");
+    perf.open("perf1.csv");
+  } else if (policy_no == 2) { // Clients
+    pinning.open("pinning2.csv");
+    perf.open("perf2.csv");
+  } else if (policy_no == 3) { // Keys
+    pinning.open("pinning3.csv");
+    perf.open("perf3.csv");
+  } else { // Random
+    pinning.open("pinning4.csv");
+    perf.open("perf4.csv");
+  }
+  std::string line;
+  for (size_t i = 0; i < second_check.size(); i++) {
+    for (size_t j = 0; j < threshold_discard.size(); j++) {
+      pinning << "Heuristics: " << i << "," << j << "\n";
+      perf << "Heuristics: " << i << "," << j << "\n";
+      prep_for_trace_read(policy_no);
+      size_t perf_counter = 0;
+      size_t current_second = 0;
+      if (trace.is_open()) {
+	while (getline(trace, line)) {
+	  std::vector<std::string> twitter_req = parse_line(line);
+          if (!good_operation(twitter_req[5])) {
+            continue;
+          }
+	  if (key_to_page_map.find(twitter_req[1]) == key_to_page_map.end()) {
+	    perf_counter += add_key();
+	  } else {
+	    update_policy_info(policy_no);
+	  }
+
+          /*Time to check!*/
+          if (convert_to_sizet(twitter_req[0]) == current_second) {
+	    current_second += second_check[i];
+	    std::vector<size_t> discarded_pages = remove_pages(threshold_discard[j]);
+	    pinning << twitter_req[0];
+	    for (size_t k = 0; k < discarded_pages.size(); k++) {
+	      pinning << "," << discarded_pages[k];
+	    }
+	    pinning << '\n';
+	    perf << convert_to_sizet(twitter_req[0]) << "," << perf_counter << "," << discarded_pages.size() << "\n";
+          }
+	}
+      }
+
+    }
+  }
+  page_to_key_map.clear();
+  key_to_page_map.clear();
+  perf.close();
+  pinning.close();
+  trace.close();
+  return 0;
+}
+
 int main(int argc, char* argv[]) { // Args: trace_file, pgsize, num_pages
   /*1. Intake a twitter trace file and register a set of memory for the memory to be placed into*/
   // Register memory
-  assign_pages();
-  /* Analyze offline the "hot" keys over time */
-  //offline_hot_key_distribution(argv[1]);
-  /* Place the elements of the twitter trace file into registered memory*/
-  //std::unordered_map<std::string, size_t> kv_store = fill_pinned_pages(argv[1], buffer);
-  
   /* Test 4 different policies here. */
+  assign_pages();
   test_policy_one(); // LRU
-  //test_policy_two(); // Client
-  //test_policy_three(); // Hot keys
+  assign_pages();
+  test_policy_two(); // Client
+  assign_pages();
+  test_policy_three(); // Hot keys
   assign_pages();
   test_policy_four(); // Random
+  //offline_hot_key_distribution();
   return 0;
 }
 
@@ -99,8 +198,6 @@ int test_policy_one() { // LRU
   perf.open("perf.csv");
   std::string line;
 
-  std::vector<size_t> second_check = {10};
-  std::vector<size_t> threshold_discard = {1, 2};
   for (size_t i = 0; i < second_check.size(); i++) {
     for (size_t j = 0; j < threshold_discard.size(); j++) {
       pinning << i << "," << j << "\n";
@@ -113,12 +210,12 @@ int test_policy_one() { // LRU
 
       size_t current_second = 5;
       if (trace.is_open()) {
+	size_t perf_counter = 0;
         while (getline(trace, line)) {
           std::vector<std::string> twitter_req = parse_line(line);
 	  if (twitter_req[5].compare("gets") != 0) {
 	    continue;
 	  }
-	  size_t perf_counter = 0; 
 	  if (key_to_page_map.find(twitter_req[1]) == key_to_page_map.end()) {
 	    size_t room = 0;
             for (auto it = page_to_key_map.begin(); it != page_to_key_map.end(); ++it) {
@@ -163,6 +260,7 @@ int test_policy_one() { // LRU
 	    }
 	    perf << convert_to_sizet(twitter_req[0]) << "," << perf_counter << "," << counter << "\n";
             unseen_pages = seen_pages;
+	    perf_counter = 0;
 	  }
         }
       }
@@ -185,8 +283,6 @@ int test_policy_four() { // LRU
   perf.open("perf_rand.csv");
   std::string line;
 
-  std::vector<size_t> second_check = {10};
-  std::vector<size_t> threshold_discard = {1};
   for (size_t i = 0; i < second_check.size(); i++) {
     for (size_t j = 0; j < threshold_discard.size(); j++) {
       pinning << i << "," << j << "\n";
@@ -254,76 +350,188 @@ int test_policy_four() { // LRU
   return 0;
 }
 
-/*int test_policy_two() { // Client
+int test_policy_two() { // Client
   std::ifstream trace;
+  std::ofstream pinning;
+  std::ofstream perf;
   trace.open(trace_file);
+  pinning.open("pinning_client.csv");
+  perf.open("perf_client.csv");
   std::string line;
-  std::vector<size_t> second_check = {1, 5, 10};
-  std::vector<size_t> threshold_discard = {1, 5, 10};
+  std::map<size_t, size_t> client_map;
+  size_t perf_counter = 0;
   for (size_t i = 0; i < second_check.size(); i++) {
     for (size_t j = 0; j < threshold_discard.size(); j++) {
+      pinning << i << "," << j << "\n";
+      perf << i << "," << j << "\n";
+      size_t current_second = 0;
+      for (auto it=client_to_page_map.begin(); it!=client_to_page_map.end(); ++it) { 
+        client_map.insert(std::pair<size_t, size_t>(it->first, 0));
+      }
       if (trace.is_open()) {
         while (getline(trace, line)) {
           std::vector<std::string> twitter_req = parse_line(line);
-          // Important: key - 1, key_size - 2, val_size - 3, op - 5
-          if (std::find(hot_keys.begin(), hot_keys.end(), twitter_req[1]) == hot_keys.end()) {
-            hot_keys.push_back(twitter_req[1]);
-            key_frequency.insert(std::pair<std::string, size_t>(twitter_req[1], 1));
-          } else {
-            key_frequency[twitter_req[1]] += 1;
-          }
-
-          if (convert_to_sizet(twitter_req[0]) % second_check[i] == 0) {
-
-            for (size_t k = 0; k < threshold_discard[j]; k++) {
-              size_t pg = rand() % number_of_pages + 1;
-            }
-          }
+	  if (twitter_req[5].compare("gets") != 0) {
+	    continue;
+	  }
+	  if (client_map.find(convert_to_sizet(twitter_req[1])) == client_map.end()) {
+	    client_map[convert_to_sizet(twitter_req[1])] += 1; 
+	  }
+	  if (key_to_page_map.find(twitter_req[1]) == key_to_page_map.end()) {
+	    size_t room = 0;
+            for (auto it = page_to_key_map.begin(); it != page_to_key_map.end(); ++it) {
+	      if (it->second.size() < MAX_ENTRIES) {
+		it->second.push_back(twitter_req[1]);
+	        key_to_page_map.insert(std::pair<std::string, size_t>(twitter_req[1], it->first));
+	        room = 1;
+		break;
+	      }
+	    }  
+	    if (room == 0) {
+	      number_of_pages += 1;
+	      perf_counter += 1;
+	      std::vector<std::string> new_vec;
+	      new_vec.push_back(twitter_req[1]);
+	      page_to_key_map.insert(std::pair<size_t, std::vector<std::string>>(number_of_pages, new_vec));
+	      key_to_page_map.insert(std::pair<std::string, size_t>(twitter_req[1], number_of_pages));
+	    }
+	  }
+	  // Important: key - 1, key_size - 2, val_size - 3, op - 5
+          if (convert_to_sizet(twitter_req[0]) == current_second) {
+	    current_second += second_check[i];
+	    size_t counter = 0;
+	    std::map<size_t, size_t> sorted_client = flip_map(client_map);
+	    for (std::map<size_t, size_t>::iterator it=sorted_client.begin(); it!=sorted_client.end(); ++it) {
+              counter += 1;
+              size_t discarded_page = client_to_page_map[it->second];
+              std::vector<std::string> keys = page_to_key_map[discarded_page];	     
+	      for (size_t h = 0; h < keys.size(); h++) {
+                key_to_page_map.erase(keys[h]);
+              }
+	      page_to_key_map.erase(discarded_page);
+	      number_of_pages -= 1;
+	      pinning << convert_to_sizet(twitter_req[0]) << "," <<  discarded_page << "\n" ;
+	      if (counter > threshold_discard[j]) {
+	        break;
+	      }
+	    }
+	    /*for (size_t k = 0; k < threshold_discard[j]; k++) {
+	      std::cout << "Number: " << number_of_pages << std::endl;
+	      if (number_of_pages == 0) {
+	        break;
+	      }
+	      size_t discarded_page = rand() % number_of_pages + 1;
+	      std::cout << "Discarded page " << discarded_page << " second " << twitter_req[0]  << std::endl;
+	      std::vector<std::string> keys = page_to_key_map[discarded_page];
+	      for (size_t h = 0; h < keys.size(); h++) {
+	        key_to_page_map.erase(keys[h]);
+	      }
+	      page_to_key_map.erase(discarded_page);
+	      counter += 1;
+	      number_of_pages -= 1;
+	      std::cout << convert_to_sizet(twitter_req[0]) << " " << page_to_key_map.size() << std::endl;
+              pinning << convert_to_sizet(twitter_req[0]) << "," <<  discarded_page << "\n" ;	      
+	      if (counter > threshold_discard[j]) {
+	        break;
+	      }
+	    }*/
+	    perf << convert_to_sizet(twitter_req[0]) << "," << perf_counter << "," << counter << "\n";
+	    perf_counter = 0;
+	  }
         }
       }
     }
   }
+  perf.close();
+  pinning.close();
   trace.close();
   return 0;
-}*/
+}
 
-/*int test_policy_three() { // Hot Key
+int test_policy_three() { // Hot Key
   std::ifstream trace;
+  std::ofstream pinning;
+  std::ofstream perf;
   trace.open(trace_file);
+  pinning.open("pinning_key.csv");
+  perf.open("perf_key.csv");
   std::string line;
-  std::vector<size_t> second_check = {1, 5, 10};
-  std::vector<size_t> threshold_discard = {1, 5, 10};
+  std::unordered_map<std::string, size_t> key_map;
   for (size_t i = 0; i < second_check.size(); i++) {
     for (size_t j = 0; j < threshold_discard.size(); j++) {
+      pinning << i << "," << j << "\n";
+      perf << i << "," << j << "\n";
+      size_t current_second = 0;
       if (trace.is_open()) {
+	std::cout << "Here! Trace is open" << std::endl;
+	size_t perf_counter = 0;
         while (getline(trace, line)) {
           std::vector<std::string> twitter_req = parse_line(line);
-          // Important: key - 1, key_size - 2, val_size - 3, op - 5
-          if (std::find(hot_keys.begin(), hot_keys.end(), twitter_req[1]) == hot_keys.end()) {
-            hot_keys.push_back(twitter_req[1]);
-            key_frequency.insert(std::pair<std::string, size_t>(twitter_req[1], 1));
-          } else {
-            key_frequency[twitter_req[1]] += 1;
-          }
-
-          if (convert_to_sizet(twitter_req[0]) % second_check[i] == 0) {
-
-            for (size_t k = 0; k < threshold_discard[j]; k++) {
-              size_t pg = rand() % NUM_PAGES + 1;
-            }
-            std::vector<std::string> old_hot_keys;
-            copy(hot_keys.begin(), hot_keys.end(), back_inserter(old_hot_keys));
-            offline_hot_keys.insert(std::pair<size_t, std::vector<std::string>>(curr_sec, old_hot_keys));
-            curr_sec = convert_to_sizet(twitter_req[0]);
-            hot_keys.clear();
-          }
+	  if (twitter_req[5].compare("gets") != 0) {
+	    continue;
+	  }
+	  if (key_map.find(twitter_req[1]) == key_map.end()) {
+            key_map.insert(std::pair<std::string, size_t>(twitter_req[1], 1));
+	  } else {
+	    key_map[twitter_req[1]] += 1; 
+	  }
+	  if (key_to_page_map.find(twitter_req[1]) == key_to_page_map.end()) {
+	    size_t room = 0;
+            for (auto it = page_to_key_map.begin(); it != page_to_key_map.end(); ++it) {
+	      if (it->second.size() < MAX_ENTRIES) {
+		it->second.push_back(twitter_req[1]);
+	        key_to_page_map.insert(std::pair<std::string, size_t>(twitter_req[1], it->first));
+	        room = 1;
+		break;
+	      }
+	    }  
+	    if (room == 0) {
+	      std::cout << "Pages replace!" << std::endl;
+	      number_of_pages += 1;
+	      perf_counter += 1;
+	      std::vector<std::string> new_vec;
+	      new_vec.push_back(twitter_req[1]);
+	      page_to_key_map.insert(std::pair<size_t, std::vector<std::string>>(number_of_pages, new_vec));
+	      key_to_page_map.insert(std::pair<std::string, size_t>(twitter_req[1], number_of_pages));
+	    }
+	  }
+	  // Important: key - 1, key_size - 2, val_size - 3, op - 5
+          if (convert_to_sizet(twitter_req[0]) == current_second) {
+            current_second += second_check[i];
+            size_t counter = 0;
+	    std::cout << "Second: " << current_second << std::endl;
+	    for (size_t k = 0; k < threshold_discard[j]; k++) {
+	      std::cout << "Number: " << number_of_pages << std::endl;
+	      if (number_of_pages == 0) {
+	        break;
+	      }
+	      size_t discarded_page = rand() % number_of_pages + 1;
+	      std::cout << "Discarded page " << discarded_page << " second " << twitter_req[0]  << std::endl;
+	      std::vector<std::string> keys = page_to_key_map[discarded_page];
+	      for (size_t h = 0; h < keys.size(); h++) {
+	        key_to_page_map.erase(keys[h]);
+	      }
+	      page_to_key_map.erase(discarded_page);
+	      counter += 1;
+	      number_of_pages -= 1;
+	      std::cout << convert_to_sizet(twitter_req[0]) << " " << page_to_key_map.size() << std::endl;
+              pinning << convert_to_sizet(twitter_req[0]) << "," <<  discarded_page << "\n" ;	      
+	      if (counter > threshold_discard[j]) {
+	        break;
+	      }
+	    }
+	    perf << convert_to_sizet(twitter_req[0]) << "," << perf_counter << "," << counter << "\n";
+	    perf_counter = 0;
+	  }
         }
       }
     }
   }
+  perf.close();
+  pinning.close();
   trace.close();
   return 0;
-}*/
+}
 
 /*int test_policy_four() { // Random
   srand (time(NULL));
@@ -439,7 +647,8 @@ size_t count_lines(std::string trace_file) {
   return count;
 }
 
-std::vector<std::string> parse_line(std::string line) {
+std::vector<std::string> parse_line(std::string line) { 
+  // twitter req: 0: timestamp, 1: key, 2: ksize, 3: vsize, 4: cid, 5: op, 6: ttl 
   std::string delim = ",";
   size_t pos = 0;
   std::string token;
